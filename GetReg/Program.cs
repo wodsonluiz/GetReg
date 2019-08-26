ade5d40f32f95053a32d074d70cc433f1128c0c8
Wodson Correia <wodson.correia@bemobi.com.br>
2019-08-26 10:37:53 -0300

    Hello World Criar e Consumir Registro

---
+++ GetReg/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Win32;
+
+namespace GetReg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The name of the key must include a valid root.
+            const string userRoot = "HKEY_CURRENT_USER";
+            const string subkey = "RegistrySetValueExample";
+            const string keyName = userRoot + "\\" + subkey;
+            
+            Registry.SetValue(keyName, "TesteWod_Registro", 1234, RegistryValueKind.QWord);
+
+           var value =  Registry.GetValue(keyName,"TesteWod_Registro", null).ToString();
+        }
+    }
+}
